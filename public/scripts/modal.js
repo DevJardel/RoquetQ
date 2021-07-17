e7c73d8c70661870325fb29b223b5812d9f7f5ce
Jardel Gomes <jardel.cod@gmail.com>
2021-07-17 11:13:46 -0300

    Start project

---
+++ public/scripts/modal.js
@@ -0,0 +1,20 @@
+export default function Modal() {
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+
+    const calcelButton = document.querySelector(".cancel")
+    calcelButton.addEventListener('click', close)
+
+    function open() {
+        modalWrapper.classList.add('active')
+    }
+    
+    function close() {
+        modalWrapper.classList.remove('active')
+    }
+
+    return {
+        open,
+        close
+    }
+}
